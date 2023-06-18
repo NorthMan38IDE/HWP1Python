d6e9c130b2e08ba6dca32d8c65bf403ad8baec8f
NorthMan38IDE <dr.laitman@mail.ru>
2023-06-18 14:37:36 +0700

    EX2 Completed

--- Ex2.py
+++ Ex2.py
@@ -2,7 +2,12 @@
 # *Пример:*
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+import sys
+
 num = int(input("Введите трехзначное число --> "))
+if num <= 0 or num >= 1000:
+    print("Invalid number")
+    sys.exit()
 tmp = 0
 while num > 0:
     tmp += num % 10
